2ff34ab41f2559a875ee1216ef96d2bab506c5c2
Assibey1456 <assibeyoppong1456@gmail.com>
2026-04-05 02:16:55 -0400

    add android platform and dist build

---
+++ aesl-website-main/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aesl.app',
+  appName: 'YourAppName',
+  webDir: 'dist'
+};
+
+export default config;
